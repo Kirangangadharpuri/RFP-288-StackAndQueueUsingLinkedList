81ac789b2b3f9e6a7303bb1fbf0b9dd2f5a6d318
Kiran <kiranpuri532@gmail.com>
2023-07-08 17:08:21 +0530

    [Kiran]Add:\ push data in satck

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1-Push data in stack");
+                Console.WriteLine("Choose above option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        StackDemo stackDemo = new StackDemo();
+                        stackDemo.Push(70);
+                        stackDemo.Push(30);
+                        stackDemo.Push(56);
+                        stackDemo.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Invalide option");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StackAndQueue/StackAndQueue/StackDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackDemo
+    {
+        public Node top;
+        public StackDemo()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if(top == null)
+            {
+                node.next=null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            Console.WriteLine("Elements in stack : "+data);
+           
+        }
+        public void Display()
+        {
+            Node temp = top;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+}
