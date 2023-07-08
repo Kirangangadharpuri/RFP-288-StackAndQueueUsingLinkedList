293bf1d084c79ed0c0313ea250273265416e0fa8
Kiran <kiranpuri532@gmail.com>
2023-07-08 17:35:09 +0530

    [Kiran]Add:\ Enque data in Queue

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -14,10 +14,11 @@ namespace StackAndQueue
             {
                 Console.WriteLine("1-Push data in stack");
                 Console.WriteLine("2- Peek and Pop of stack");
+                Console.WriteLine("3-Enque data in Queue");
                 Console.WriteLine("Choose above option");
                 int option = Convert.ToInt32(Console.ReadLine());
                 StackDemo stackDemo = new StackDemo();
-
+                QueueDemo queueDemo = new QueueDemo();
                 switch (option)
                 {
                     case 1:
@@ -38,6 +39,13 @@ namespace StackAndQueue
                         Console.WriteLine("#############");
                         stackDemo.Display();
                         break;
+                    case 3:
+                        queueDemo.Enque(56);
+                        queueDemo.Enque(30);
+                        queueDemo.Enque(70);
+                        //queueDemo.Display();
+                        break;
+
                     default:
                         Console.WriteLine("Invalide option");
                         break;
